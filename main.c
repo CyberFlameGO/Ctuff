c78fbd73aeccf2e77741b7201fe794ff437d316b
Aaron Lowe <cyberflameu@gmail.com>
2021-09-27 01:22:55 +1300

    oooooooh

--- main.c
+++ main.c
@@ -6,5 +6,11 @@ int main() {
     scanf("%d", &testvar);
     printf("%d", testvar);
     getchar();
+    if(testvar > 9) {
+        printf("please say this is like java");
+    }
+    else if(testvar > 8)
+        printf("one liners???");
+    else;
     return 0;
 }
