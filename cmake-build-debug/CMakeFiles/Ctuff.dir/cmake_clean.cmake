f9ada9669b267a95cd859ef18e0b60febcc98d71
Aaron Lowe <cyberflameu@gmail.com>
2021-09-27 01:03:24 +1300

    init
    Signed-off-by: Aaron Lowe <cyberflameu@gmail.com>

---
+++ cmake-build-debug/CMakeFiles/Ctuff.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ctuff.dir/main.c.o"
+  "Ctuff"
+  "Ctuff.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ctuff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
